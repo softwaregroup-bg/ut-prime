4a8de3ebe7b12d338a08970b4d0471f274bdd9c8
Kalin Krustev <kalin.krustev@softwaregroup.com>
2021-01-28 11:59:14 +0200

    test: turn on coverage

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,17 @@
+const notUt = '/node_modules/(?!(impl|ut)-)';
 module.exports = {
     roots: ['./src'],
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx}',
+        '!*.stories.tsx'
+    ],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['lcov', 'cobertura', 'text'],
     setupFilesAfterEnv: ['./jest.setup.js'],
-    coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '/node_modules/(?!(impl|ut)-)'],
-    testPathIgnorePatterns: ['<rootDir>/node_modules/', '/node_modules/(?!(impl|ut)-)'],
-    transformIgnorePatterns: ['<rootDir>/node_modules/(?!(impl|ut)-)', '/node_modules/(?!(impl|ut)-)'],
+    coveragePathIgnorePatterns: ['<rootDir>/node_modules/', notUt],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', notUt],
+    transformIgnorePatterns: ['<rootDir>/node_modules/(?!(impl|ut)-)', notUt],
     moduleNameMapper: {
         '^.+\\.(css|less|scss|png)$': 'babel-jest'
     }
