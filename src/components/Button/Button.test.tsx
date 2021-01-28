ef0b7b46d440864d015b5e5439b32dbc18834e63
Kalin Krustev <kalin.krustev@softwaregroup.com>
2021-01-28 11:36:20 +0200

    test: fix button test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 
+import { render } from '../test';
 import Button from './index';
 
 describe('<Button />', () => {
     it('render equals snapshot', () => {
         const { getByTestId } = render(<Button />);
-
-        expect(getByTestId('Button')).toMatchSnapshot();
+        expect(getByTestId('ut-front-test')).toMatchSnapshot();
     });
 
     it('triggers sent onClick function', () => {
         const onClick = jest.fn();
-
-        const { getByTestId } = render(<Button onClick={onClick} />);
-
+        const { getByTestId } = render(<Button data-testid='Button' onClick={onClick} />);
         fireEvent.click(getByTestId('Button'));
-
         expect(onClick).toHaveBeenCalledTimes(1);
     });
 });
