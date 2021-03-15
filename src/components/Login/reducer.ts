d1c7f2c5b6d6d6c7f81c25d12c07fcdaed37aadb
Kalin Krustev <kalin.krustev@softwaregroup.com>
2021-03-15 17:44:13 +0200

    fix: login store

--- src/components/Login/reducer.ts
+++ src/components/Login/reducer.ts
@@ -1,9 +1,13 @@
 import {LOGIN, COOKIE_CHECK} from './actions';
 import Immutable from 'immutable';
 
-export default (state = null, action: { type: Symbol; methodRequestState: string; result: null; }) => {
+export default (state = null, action: { type: Symbol; methodRequestState: string; result: {language: {}}; }) => {
     if ([LOGIN, COOKIE_CHECK].includes(action.type) && action.methodRequestState === 'finished') {
-        return action.result ? Immutable.fromJS(action.result) : false;
+        //! todo simplify
+        return action.result ? Immutable.fromJS({
+            result: action.result,
+            language: action.result.language
+        }) : false;
     }
     return state;
 };
