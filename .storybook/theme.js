2d7ffd7146dd0c2b00ce318a233344805fabddf8
Kalin Krustev <kalin.krustev@softwaregroup.com>
2021-04-02 12:52:35 +0300

    docs: storybook theme

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+import brandImage from './ut.png'
+
+export default create({
+  base: 'dark',
+  brandTitle: 'UT',
+  brandUrl: 'https://github.com/softwaregroup-bg',
+  brandImage,
+});
