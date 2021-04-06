9c6f2ba295d08d3133dc64d86a14ba73da309f01
Kalin Krustev <kalin.krustev@softwaregroup.com>
2021-04-06 14:35:52 +0300

    chore: align with docs

--- src/components/dx/index.ts
+++ src/components/dx/index.ts
@@ -1,1 +1,11 @@
+export { default as Box} from 'devextreme-react/box';
+export { default as Button } from 'devextreme-react/button';
+export { default as CustomStore} from 'devextreme/data/custom_store';
+export { default as DataGrid} from 'devextreme-react/data-grid';
+export { default as DataSource} from 'devextreme/data/data_source';
+export { default as Drawer} from 'devextreme-react/drawer';
+export { default as Popup } from 'devextreme-react/popup';
+export { default as Toolbar} from 'devextreme-react/toolbar';
+
+// overridden
 export { default as PieChart } from './PieChart';
--- src/components/mui/index.ts
+++ src/components/mui/index.ts
@@ -1,4 +1,6 @@
-export { default as Button } from './Button';
-export { default as Dialog } from './Dialog';
-export { default as Grid } from './Grid';
+export { default as Button } from '@material-ui/core/Button';
+export { default as Dialog } from '@material-ui/core/Dialog';
+export { default as Grid } from '@material-ui/core/Grid';
+
+// overridden
 export { default as TextField } from './TextField';
