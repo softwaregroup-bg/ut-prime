3f4e9c56b6169ab958057af7df20c7f362e85576
Kalin Krustev <kalin.krustev@gmail.com>
2022-07-19 13:25:55 +0300

    test: await submit

--- src/components/Editor/Editor.stories.tsx
+++ src/components/Editor/Editor.stories.tsx
@@ -114,7 +114,7 @@ Validation.play = async({canvasElement}) => {
     await canvas.findByDisplayValue('Oak'); // wait for the data to be loaded
     await userEvent.clear(canvas.getByLabelText('Name'));
     await userEvent.click(canvas.getByLabelText('save'));
-    await canvas.findByLabelText('Name', {selector: '.p-invalid'});
+    await canvas.findByRole('textbox', {name: 'Name', selector: '.p-invalid'});
 };
 
 const serverError = () => {
