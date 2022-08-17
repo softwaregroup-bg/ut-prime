00387554631b8c158a1b6b19afe8f4784b472f22
Kalin Krustev <kalin.krustev@gmail.com>
2022-08-17 18:17:56 +0300

    chore: update generator

--- generator/component-template/component.index.js
+++ generator/component-template/component.index.js
@@ -1,13 +1,21 @@
 module.exports = (componentName) => ({
     content: `import React from 'react';
 import clsx from 'clsx';
+import {createUseStyles} from 'react-jss';
+import type {ComponentProps} from './${componentName}.types';
 
-import { useStyles, ComponentProps } from './${componentName}.types';
+const useStyles = createUseStyles({
+    ${componentName.toLowerCase()}: {
+    }
+});
 
-const ${componentName}: ComponentProps = ({ className, ...rest }) => {
+const ${componentName}: ComponentProps = ({
+    className,
+    ...rest
+}) => {
     const classes = useStyles();
     return <div
-        className={clsx(classes.component, className)}
+        className={clsx(classes.${componentName.toLowerCase()}, className)}
         {...rest}
     />;
 };
--- generator/component-template/component.types.js
+++ generator/component-template/component.types.js
@@ -1,15 +1,9 @@
 module.exports = () => ({
-    content: `import {createUseStyles} from 'react-jss';
-import React from 'react';
+    content: `import React from 'react';
 
 export interface Props {
     className?: string;
 }
-
-export const useStyles = createUseStyles({
-    component: {
-    }
-});
 
 export type ComponentProps = React.FC<Props>
 `,
