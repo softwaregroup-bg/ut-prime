5e20f2b75c67ccca1adaca7230e04720acc1babc
Kalin Krustev <kalin.krustev@gmail.com>
2022-09-29 14:18:41 +0300

    chore: deps

--- .storybook/theme.js
+++
@@ -1,9 +0,0 @@
-import { create } from '@storybook/theming';
-import brandImage from './ut.png'
-
-export default create({
-  base: 'dark',
-  brandTitle: 'UT',
-  brandUrl: 'https://github.com/softwaregroup-bg',
-  brandImage,
-});
