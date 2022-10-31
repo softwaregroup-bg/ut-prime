65200a8bf88d99fcb6bff05e144a1aad126b5d4d
Kalin Krustev <kalin.krustev@gmail.com>
2022-10-31 12:53:54 +0200

    feat: json diff / view initial code

---
+++ src/components/Json/styles.ts
@@ -0,0 +1,11 @@
+import {createUseStyles} from 'react-jss';
+
+export const useStyles = createUseStyles({
+    primary: {
+        color: 'var(--primary-color)'
+    },
+    spaces: {
+        whiteSpace: 'pre-wrap',
+        display: 'inline-block'
+    }
+});
