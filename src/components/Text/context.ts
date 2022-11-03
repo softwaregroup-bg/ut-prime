5279ad40345a9c7b39d829884504edb193d2e596
goodlifeinc <genata93@gmail.com>
2022-11-03 11:07:41 +0200

    fix: PortalConfiguration type

--- src/components/Text/context.ts
+++ src/components/Text/context.ts
@@ -2,14 +2,14 @@ import React from 'react';
 import type { GMapOptions } from '../prime/googlemap/GMap.types';
 
 export interface PortalConfiguration {
-    'portal.utPrime.GMap'?: GMapOptions
+    'portal.utPrime.GMap'?: string | GMapOptions;
 }
 
 interface contextType {
     language?: string;
     languageCode?: string;
-    translate?: (id: string, text:string, language: string) => string,
-    configuration?: PortalConfiguration
+    translate?: (id: string, text: string, language: string) => string;
+    configuration?: PortalConfiguration;
 }
 const defaultContext: contextType = {};
 export default React.createContext(defaultContext);
