fe7e635db779aed8a2c4a914ba190d9f589e3103
annamardesign <anna.marinova@softwaregroup.com>
2024-10-29 15:40:08 +0200

    fix: label update (#82)

--- src/components/lib/column.tsx
+++ src/components/lib/column.tsx
@@ -179,7 +179,11 @@ export default function columnProps({
                 name={filterId}
             />;
             body = function body(rowData) {
-                return rowData[property?.body || fieldName];
+                if (property?.body) {
+                    return rowData[property?.body];
+                }
+                const item = dropdowns?.[dropdown]?.find(({value}) => value === rowData[fieldName]);
+                return item?.label || rowData[fieldName];
             };
             break;
         case 'multiSelect':
